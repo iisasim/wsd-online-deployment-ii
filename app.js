275fc2277f91974dd9d1b23cfc60361fdc83bcff
unknown <iisa.simola@gmail.com>
2023-09-18 15:26:24 +0300

    nettisivu

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import { serve } from "./deps.js";
-import { configure, renderFile } from "./deps.js";
+import { serve } from "https://deno.land/std@0.171.0/http/server.ts";
+import { configure, renderFile } from "https://deno.land/x/eta@v2.0.0/mod.ts";
 
 configure({
   views: `${Deno.cwd()}/views/`,
@@ -9,18 +9,12 @@ const responseDetails = {
   headers: { "Content-Type": "text/html;charset=UTF-8" },
 };
 
-const data = {
-  count: 0,
-};
-
 const handleRequest = async (request) => {
-  const url = new URL(request.url);
-  if (url.pathname === "/count") {
-    data.count++;
-    return new Response(await renderFile("count.eta", data), responseDetails);
+  if (request.method === "POST") {
+    console.log(request.headers.get("Content-Type"));
   }
 
-  return new Response("Hello you!");
+  return new Response(await renderFile("index.eta"), responseDetails);
 };
 
 serve(handleRequest, { port: 7777 });
