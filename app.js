c2bc2cbd2b013bffcef7db13353d7108af5f5eb2
unknown <iisa.simola@gmail.com>
2023-09-18 15:20:11 +0300

    palautus

--- app.js
+++ app.js
@@ -1,44 +1,26 @@
 import { serve } from "./deps.js";
-import { renderFile } from "/deps.js";
+import { configure, renderFile } from "./deps.js";
 
-const PORT = 7777;
+configure({
+  views: `${Deno.cwd()}/views/`,
+});
 
 const responseDetails = {
   headers: { "Content-Type": "text/html;charset=UTF-8" },
 };
 
 const data = {
-  visits: 0,
+  count: 0,
 };
 
 const handleRequest = async (request) => {
   const url = new URL(request.url);
-
-  if (url.pathname === "/visits") {
-    data.visits++;
-    return new Response(
-      await renderFile(`${Deno.cwd()}/views/visits.eta`, data),
-      responseDetails
-    );
+  if (url.pathname === "/count") {
+    data.count++;
+    return new Response(await renderFile("count.eta", data), responseDetails);
   }
 
-  if (url.pathname === "/meaning") {
-    return new Response(
-      "Seeking truths beyond meaning of life, you will find 43.",
-      responseDetails
-    );
-  }
-
-  return new Response("Nothing here yet.", responseDetails);
+  return new Response("Hello you!");
 };
 
-const server = serve({ port: PORT });
-
-console.log(`Server is running on http://localhost:${PORT}/`);
-
-for await (const request of server) {
-  const response = await handleRequest(request);
-  request.respond(response);
-}
-
-
+serve(handleRequest, { port: 7777 });
